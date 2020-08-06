511f75950c338abb2f0b96ac12afbe003e64de6b
Jiwon Kang <jwkorange@gmail.com>
2020-08-06 16:42:23 +0900

    [Add] 11726

---
+++ src/BaekJoon/11726.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+const int MAX = 1001;
+int dp[MAX];
+
+int func(int num)
+{
+	if (num == 1) return 1;
+	if (num == 2) return 2;
+	if (dp[num] != -1) return dp[num] % 10007;
+
+	dp[num] = (func(num - 1) + func(num - 2)) % 10007;
+
+	return dp[num];
+}
+int main()
+{
+	int N;
+	std::cin >> N;
+
+	std::fill(dp, dp + MAX, -1);
+
+	std::cout << func(N);
+}
