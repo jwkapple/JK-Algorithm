6d858838e6acf98b5d36e53a1b5178da771d323a
Jiwon Kang <jwkorange@gmail.com>
2020-08-10 17:27:53 +0900

    [Add] 1309

---
+++ src/BaekJoon/1309.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int N;
+int left, right, empty;
+int fLeft, fRight, fEmpty;
+
+int main()
+{
+	std::cin >> N;
+
+	left = right = empty = 1;
+
+	for (int i = 0;i < N - 1; i++)
+	{
+		fLeft = (right + empty) % 9901;
+		fRight = (left + empty) % 9901;
+		fEmpty = (left + right + empty) % 9901;
+
+		right = fRight % 9901;
+		left = fLeft % 9901;
+		empty = fEmpty % 9901;
+	}
+
+	std::cout << (left + right + empty) % 9901;
+}
