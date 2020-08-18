ea38d3833d9813dca5aed5b5dfe63c13f223ed5d
Jiwon Kang <jwkorange@gmail.com>
2020-08-18 13:24:27 +0900

    [Add] 1182

---
+++ src/BaekJoon/1182.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+const int MAX = 21;
+
+int data[MAX];
+int N, S;
+int result = 0;
+int currentSum = 0;
+
+void back(int current)
+{
+	if (current == N) return;
+
+	if (currentSum + data[current] == S) result++;
+
+	back(current + 1);
+
+	currentSum += data[current];
+
+	back(current + 1);
+
+	currentSum -= data[current];
+}
+int main()
+{
+	std::cin >> N >> S;
+
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> data[i];
+	}
+
+	back(0);
+
+	std::cout << result;
+}
