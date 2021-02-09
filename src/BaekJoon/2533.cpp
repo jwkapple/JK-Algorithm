da4f44ff2278948aaaf9969bc9ff129c203807ee
Jiwon Kang <jwkorange@gmail.com>
2021-02-09 11:17:00 +0900

    [Add] 17143 BFS

--- src/BaekJoon/2533.cpp
+++ src/BaekJoon/2533.cpp
@@ -6,49 +6,107 @@ void Init()
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL); std::cout.tie(NULL);
 }
- 
-const int MAX = 1e6 + 2;
 
-std::vector<int> child[MAX];
-bool visited[MAX], EA[MAX];
-int N, result = 0;
-
-bool func(int cur)
+struct Node
 {
-	visited[cur] = true;
-	auto size = child[cur].size();
-	if (size == 1 && cur != 1) return false;
+	int R;
+	int C;
+	int S;
+	int D;
+	int Z;
 
-	auto& ret = EA[cur];
-	ret = true;
+	Node(int r, int c, int s, int d, int z) : R(r), C(c), S(s), D(d), Z(z) {}
+};
 
-	for (int i = 0; i < size; ++i)
+enum dir = { NONE = 0, UP, DOWN, RIGHT, LEFT };
+
+const int MAX = 100 + 1;
+
+std::vector<Node> data;
+int map[MAX][MAX];
+int R, C, M, King = 0, result = 0;
+
+void move(int i)
+{
+	auto&[r, c, s, d, z] = data[i];
+	
+	int speed;
+	
+	switch (d)
 	{
-		if (visited[child[cur][i]]) continue;
-		ret &= func(child[cur][i]);
+	case UP:
+		speed = s % (R * 2);
+		if (speed < r) r -= speed;
+		else { r = speed - r; d = DOWN; }
+		break;
+	case DOWN:
+		speed = s % (R * 2);
+		if (speed < R - r) r += speed;
+		else { r = 2R - speed - r; d = UP; }
+		break;
+	case RIGHT:
+		speed = s % (C * 2);
+		if (speed < C - c) c +=  speed;
+		else { c = 2C - speed - c; d = LEFT; }
+	case LEFT:
+		speed = s % (C * 2);
+		if (speed < c) c -= speed;
+		else { c = s - c; d = RIGHT; }
 	}
 
-	if (!ret) { result++; ret = true; }
-	else ret = false;
-	return ret;
+	if(data[map[r][c]].Z < z)
+	{
+		data[map[r][c]].Z = 0;
+		
+		map[r][c] = i;
+	}
+	else data[i].Z = 0;
 }
-
 int main()
 {	
 	Init();
-	
-	std::cin >> N;	 
-	
-	int from, to;	
-	for (int i = 0; i < N - 1; ++i)	 
-	{		 
-		std::cin >> from >> to;	
 
-		child[from].push_back(to);
-		child[to].push_back(from);
+	std::cin >> R >> C >> M;
+
+	int r, c, s, d, z;
+
+	data.push_back(Node(0, 0, 0, 0, 0));
+	for(int i = 1; i <= M; ++i)
+	{
+		std::cin >> r >> c >> s >> d >> z;
+
+		data.push_back(Node(r, c, s, d, z));
+		data[r][c] = i;
 	}
-	
-	func(1);
+
+	while(King != C)
+	{
+		King++;
+
+		int current = 1;
+		while(current != R)
+		{
+			auto& pos = map[current][King];
+			if(!pos)
+			{
+				auto& cur = data[pos];
+
+				result += cur.Z;
+				cur.Z = 0;
+				pos = 0;
+
+				break;
+			}
+			current++;
+		}
+
+		for(int i = 0; i < M; ++i)
+		{
+			if (!data[i].Z) continue;
+
+			move(i);
+		}
+	}
 
 	std::cout << result;
 }
