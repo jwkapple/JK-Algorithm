a521afabb13eba4170dfd3ed35c3ab5812483c8c
Jiwon Kang <jwkorange@gmail.com>
2021-03-04 14:39:23 +0900

    [Update] 2610 Floyd

--- src/BaekJoon/2610.cpp
+++ src/BaekJoon/2610.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 #include <algorithm>
+#include <vector>
 
-#define pii std::pair<int, int>
+#define Input(from, to, v) data[from][to] = v, data[to][from] = v
 
 void Init()
 {
@@ -13,38 +12,11 @@ void Init()
 
 const int MAX = 100 + 1;
 
-std::priority_queue<pii, std::vector<pii>, std::greater<pii>> pq;
-std::vector<int> data[MAX]; // 위원회 정보
 std::vector<int> result;
-int map[MAX][MAX];
-int visited[MAX];
+bool visited[MAX];
+int data[MAX][MAX];
+int value[MAX];
 int N, M;
-
-int func(int cur)
-{
-	int min = 102;
-	int ret = 0;
-	auto arr = data[cur];
-	int size = data[cur].size();
-
-	for (int y = 0; y < size; ++y)
-	{
-		int max = 0;
-		int Y = arr[y];
-		for (int x = 0; x < size; ++x)
-		{
-			if (y == x) continue;
-
-			int X = arr[x];
-
-			max = std::max(max, map[Y][X]);
-		}
-
-		if (min > max) { min = max; ret = Y; }
-	}
-
-	return ret;
-}
 
 int main()
 {
@@ -52,84 +24,77 @@ int main()
 
 	std::cin >> N >> M;
 
-	int from, to, current = 0;
-
+	int from, to;
 	for (int i = 0; i < M; ++i)
 	{
 		std::cin >> from >> to;
 
-		pq.push(pii(from, to));
-	}
-
-
-	for (int i = 0; i < M; ++i)
-	{
-		auto[from, to] = pq.top(); pq.pop();
-
-		map[from][to] = 1; map[to][from] = 1;
-		int F = visited[from];
-		int T = visited[to];
-		if (F)
-		{
-			if (T) continue;
-
-			data[F].push_back(to);
-			visited[to] = F;
-		}
-		else
-		{
-			if (T)
-			{
-				data[T].push_back(from);
-				visited[from] = T;
-			}
-
-			else
-			{
-				current++;
-				data[current].push_back(from); visited[from] = current;
-				data[current].push_back(to);   visited[to] = current;
-			}
-		}
-	}
-
-	for (int cur = 1; cur <= N; ++cur)
-	{
-		for (int y = 1; y <= N; ++y)
-		{
-			for (int x = 1; x <= N; ++x)
-			{
-				if (y == x || y == cur || x == cur) continue;
-				if (!map[y][cur] || !map[x][cur]) continue;
-
-				int total = map[y][cur] + map[x][cur];
-				if (!map[y][x]) { map[y][x] = total; map[x][y] = total; }
-				else
-				{
-					int ret = std::min(map[y][x], total);
-					map[y][x] = ret; map[x][y] = ret;
-				}
-
-			}
-		}
-	}
-
-	for (int i = 1; i <= current; ++i)
-	{
-		result.push_back(func(i));
+		value[from] = value[to] = 1;
+		Input(from, to, 1);
 	}
 
 	for (int i = 1; i <= N; ++i)
 	{
-		if (!visited[i]) result.push_back(i);
+		for (int y = 1; y <= N; ++y)
+		{
+			if (y == i) continue;
+			for (int x = 1; x <= N; ++x)
+			{
+				if (x == i || !data[y][i] || !data[i][x] || y == x) continue;
+
+				int nValue = data[y][i] + data[i][x];
+				if (!data[y][x])
+				{
+					Input(y, x, nValue);
+					continue;
+				}
+
+				if (data[y][x] > nValue)Input(y, x, nValue);
+			}
+		}
+	}
+
+	for (int y = 1; y <= N; ++y)
+	{
+		for (int x = 1; x <= N; ++x)
+		{
+			if (data[y][x] > value[y]) value[y] = data[y][x];
+		}
+	}
+	for (int i = 1; i <= N; ++i)
+	{
+		if (!value[i])
+		{
+			visited[i] = true;
+			result.push_back(i);
+			continue;
+		}
+
+		if (visited[i]) continue;
+
+		visited[i] = true;
+		int min = value[i];
+		int minN = i;
+		for (int j = 1; j <= N; ++j)
+		{
+			if (data[i][j])
+			{
+				visited[j] = true;
+				if (min > value[j])
+				{
+					min = value[j];
+					minN = j;
+				}
+			}
+		}
+
+		result.push_back(minN);
 	}
 
 	std::sort(result.begin(), result.end());
-
-	int size = result.size();
-	std::cout << size << "\n";
-	for (int i = 0; i < size; ++i)
+	std::cout << result.size() << "\n";
+	for (auto p : result)
 	{
-		std::cout << result[i] << "\n";
+		std::cout << p << "\n";
 	}
 }
