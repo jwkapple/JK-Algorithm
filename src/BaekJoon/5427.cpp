a09d6a3adf66519d377e33f6a28eedc7073e6a72
Jiwon Kang <jwkorange@gmail.com>
2021-03-30 10:02:24 +0900

    [Add] 5427 BFS

--- src/BaekJoon/5427.cpp
+++ src/BaekJoon/5427.cpp
@@ -1,66 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
+#include <string>
 
 #define pii std::pair<int, int>
-#define mkpii std::make_pair
+
 void Init()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL); std::cout.tie(NULL);
 }
 
-int dir[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
-const int MAX = 1000 + 1;
+enum Case { NONE = 0, WALL, FIRE, ME };
+int dir[4][2]{ {-1,0},{1,0},{0,-1},{0,1} };
+const int MAX = 1000 + 5;
 
-std::queue<pii> fireQ, SHQ;
 std::vector<int> result;
-bool visited[MAX][MAX], fire[MAX][MAX], wall[MAX][MAX], SH[MAX][MAX];
-int T, W, H;
-
-bool border(int y, int x, bool arr[MAX][MAX], bool Fire)
-{
-	if (!Fire) { Fire = !fire[y][x]; }
-	return Fire && !arr[y][x] && !wall[y][x] && y >= 0 && y < H && x >= 0 && x < W;
-}
-
-void insert(pii pos, bool arr[MAX][MAX], std::queue<pii> Q, bool Fire)
-{
-	for (int i = 0; i < 4; ++i)
-	{
-		int Y = pos.first + dir[i][0];
-		int X = pos.second + dir[i][1];
-
-		if (border(Y, X, fire, Fire))
-		{
-			arr[Y][X] = true;
-			Q.push(pii(Y, X));
-		}
-	}
-}
-void init(int w, int h)
-{
-	char tmp;
-	for (int y = 0; y < h; ++y)
-	{
-		for (int x = 0; x < w; ++x)
-		{
-			wall[y][x] = false;
-			visited[y][x] = false;
-			fire[y][x] = false;
-			SH[y][x] = false;
-
-			std::cin >> tmp;
-			switch (tmp)
-			{
-			case '#': wall[y][x] = true; break;
-			case '*': fire[y][x] = true; fire[y][x] = true; fireQ.push(mkpii(y, x)); break;
-			case '@': SH[y][x] = true; SH[y][x] = true; SHQ.push(mkpii(y, x)); break;
-			}
-		}
-	}
-}
+int map[MAX][MAX];
+bool visited[MAX][MAX], visitF[MAX][MAX];
+int T, H, W;
 
 int main()
 {
@@ -68,35 +26,116 @@ int main()
 
 	std::cin >> T;
 
+	std::string tmp;
+
 	while (T--)
 	{
+		std::queue<pii> Q, fire;
+
+		bool isDone = false;
+		int current = 0;
+
 		std::cin >> W >> H;
 
-		init(W, H);
-
-		while (!SHQ.empty())
+		for (int y = 1; y <= H; ++y)
 		{
-			int size = fireQ.size();
-			pii cur;
-			// fire case
-			for (int i = 0; i < size; ++i)
+			std::cin >> tmp;
+			for (int x = 1; x <= W; ++x)
 			{
-				auto[y, x] = fireQ.front();
-				fireQ.pop();
-
-				if (border(y, x, fire, true)) insert(cur, fire, fireQ, true);
-			}
-
-			size = SHQ.size();
-
-			// SH case
-			for (int i = 0; i < size; ++i)
-			{
-				auto[y, x] = SHQ.front();
-				SHQ.pop();
-
-				if (border(y, x, SH, false)) insert(cur, SH, SHQ, false);
+				switch (tmp[x - 1])
+				{
+				case '.': map[y][x] = NONE; break;
+				case '#': map[y][x] = WALL; break;
+				case '*': map[y][x] = FIRE; fire.push(pii(y, x)); break;
+				case '@': map[y][x] = ME;   Q.push(pii(y, x)); break;
+				}
 			}
 		}
+
+		while (!isDone)
+		{
+			int fireS = fire.size();
+
+			for (int i = 0; i < fireS; ++i)
+			{
+				auto[y, x] = fire.front(); fire.pop();
+
+				if (visitF[y][x]) continue;
+
+				visitF[y][x] = true;
+				for (int j = 0; j < 4; ++j)
+				{
+					int Y = y + dir[j][0];
+					int X = x + dir[j][1];
+
+					if (Y > 0 && Y <= H && X > 0 && X <= W)
+					{
+						if (map[Y][X] == NONE || map[Y][X] == ME)
+						{
+							if (!visitF[Y][X])
+							{
+								map[Y][X] = FIRE;
+								fire.push(pii(Y, X));
+							}
+						}
+					}
+				}
+			}
+
+			int size = Q.size();
+			if (!size)
+			{
+				result.push_back(-1);
+				isDone = true;
+				break;
+			}
+
+			for (int i = 0; i < size; ++i)
+			{
+				auto[y, x] = Q.front(); Q.pop();
+
+				if (visited[y][x]) continue;
+
+				visited[y][x] = true;
+
+				map[y][x] = ME;
+				for (int j = 0; j < 4; ++j)
+				{
+					int Y = y + dir[j][0];
+					int X = x + dir[j][1];
+
+					if (Y > 0 && Y <= H && X > 0 && X <= W)
+					{
+						if (map[Y][X] == NONE) Q.push(pii(Y, X));
+					}
+					else
+					{
+						result.push_back(current + 1);
+						isDone = true;
+						break;
+					}
+				}
+				if (isDone) break;
+			}
+
+			if (isDone) break;
+
+			current++;
+		}
+		for (int y = 0; y <= H; ++y)
+		{
+			for (int x = 0; x <= W; ++x)
+			{
+				map[y][x] = 0;
+				visited[y][x] = false;
+				visitF[y][x] = false;
+			}
+		}
+	}
+
+	for (auto p : result)
+	{
+		if (p < 0) std::cout << "IMPOSSIBLE\n";
+		else std::cout << p << "\n";
 	}
 }
