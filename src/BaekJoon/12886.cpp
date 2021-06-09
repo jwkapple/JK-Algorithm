e20f3495c08295fba33726cc4d35b3a041285a12
Jiwon Kang <jwkorange@gmail.com>
2021-06-09 10:07:01 +0900

    [Add] 12886 BFS

---
+++ src/BaekJoon/12886.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <queue>
+
+#define pii std::pair<int, int>
+void Init()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL); std::cout.tie(NULL);
+}
+
+const int MAX = 1500 + 1;
+
+std::queue<pii> Q;
+bool visited[MAX][MAX], isDone = false;
+int A, B, tot;
+
+int main()
+{
+	Init();
+
+	std::cin >> A >> B >> tot;
+
+	tot += A + B;
+
+	Q.push(pii(A, B));
+
+	int tA, tB, c;
+	while (!Q.empty() && !isDone)
+	{
+		auto[a, b] = Q.front(); Q.pop();
+
+		if (a < 0 || b < 0) continue;
+
+		c = tot - (a + b);
+		if (a == b && b == c)
+		{
+			isDone = true;
+			break;
+		}
+
+		auto& ret = visited[a][b];
+		if (ret) continue;
+		ret = true;
+
+		if (a != b)
+		{
+			tA = a > b ? a - b : a + a;
+			tB = a > b ? b + b : b - a;
+
+			if (!visited[tA][tB]) Q.push(pii(tA, tB));
+		}
+
+		if (b != c)
+		{
+			tB = c > b ? b + b : b - c;
+
+			if (!visited[a][tB])Q.push(pii(a, tB));
+		}
+
+		if (a != c)
+		{
+			tA = a > c ? a - c : a + a;
+
+			if (!visited[tA][b]) Q.push(pii(tA, b));
+		}
+	}
+
+	std::cout << isDone;
+}
