97c1736b30c4a8a91e7500f8c2e4e2a08df34ae6
Jiwon Kang <jwkorange@gmail.com>
2021-07-26 17:10:08 +0900

    [Add] 14226 DP

---
+++ src/BaekJoon/14226.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <queue>
+
+#define pii std::pair<int, int>
+
+void Init()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL); std::cout.tie(NULL);
+}
+
+const int MAX = 4e3 + 1;
+
+std::queue<pii> data; // value, clipboard
+bool visited[MAX][MAX];
+
+int S, T, C = -1, result = 0;
+
+int main()
+{
+	Init();
+
+	std::cin >> S;
+
+	data.push(pii(1, 0));
+
+	int size;
+	while (!data.empty())
+	{
+		size = data.size();
+		for (int i = 0; i < size; ++i)
+		{
+			auto[value, C] = data.front(); data.pop();
+			if (visited[value][C] || value == 0) continue;
+			visited[value][C] = true;
+			if (value == S) { result = T; break; }
+
+			if (value > S) data.push(pii(value - 1, C));
+			else
+			{
+				if (C) data.push(pii(value + C, C));
+				if (value > 0) data.push(pii(value - 1, C));
+				data.push(pii(value, value));
+			}
+		}
+
+		if (result) break;
+
+		T++;
+	}
+
+	std::cout << result;
+}
