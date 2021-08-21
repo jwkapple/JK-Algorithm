15e831459507bcdeda21c5f30f6ff1c1f8499498
DESKTOP-H0JLFMK\Glacier's Desktop <jwkorange@gmail.com>
2021-08-21 11:45:19 +0900

    [Update] 1005 Optimized

--- src/BaekJoon/1005.cpp
+++ src/BaekJoon/1005.cpp
@@ -3,78 +3,68 @@
 
 void Init()
 {
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL); std::cout.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL); std::cout.tie(NULL);
 }
 
 const int MAX = 1000 + 1;
 
 std::vector<int> parent[MAX];
 std::vector<int> result;
-int construct[MAX], prev[MAX];
-int T, N, K, finale;
+int construct[MAX], child[MAX];
+int T, N, K, W, from, to, nValue;
 
-void clear()
+int main()
 {
-	for (int i = 1; i <= N; ++i)
-	{
-		parent[i].clear();
-		construct[i] = 0, prev[i] = 0;
-	}
-}
-int main() {
+    Init();
 
-	Init();
+    std::cin >> T;
 
-	std::cin >> T;
+    while (T--)
+    {
+        std::cin >> N >> K;
+        std::vector<int> value(N + 1);
 
-	while (T--)
-	{
-		std::cin >> N >> K;
-		std::vector<int> value(N + 1);
+        for (int i = 1; i <= N; ++i) { std::cin >> value[i]; }
 
-		for (int i = 1; i <= N; ++i)
-		{
-			std::cin >> value[i];
-		}
+        for (int i = 1; i <= K; ++i)
+        {
+            std::cin >> from >> to;
+            parent[from].push_back(to);
+            child[to]++;
+        }
 
-		int from, to;
-		for (int i = 0; i < K; ++i)
-		{
-			std::cin >> from >> to;
+        std::cin >> W;
 
-			parent[from].push_back(to);
-			prev[to]++;
-		}
+        while (child[W] != -1)
+        {
+            for (int i = 1; i <= N; ++i)
+            {
+                if (child[i] == -1) continue;
 
-		std::cin >> finale;
+                if (child[i] == 0)
+                {
+                    child[i] = -1;
+                    for (auto p : parent[i])
+                    {
+                        child[p]--;
+                        nValue = construct[i] + value[i];
+                        construct[p] = construct[p] > nValue ? construct[p] : nValue;
+                    }
+                }
+            }
+        }
 
-		while (prev[finale] != -1)
-		{
-			for (int i = 1; i <= N; ++i)
-			{
-				if (prev[i] == -1) continue;
-				if (!prev[i])
-				{
-					prev[i] = -1;
-					for (int j = 0; j < parent[i].size(); ++j)
-					{
-						auto cur = parent[i][j];
-						prev[cur]--;
-						int v = value[i] + construct[i];
-						construct[cur] = v > construct[cur] ? v : construct[cur];
-					}
-				}
-			}
-		}
+        result.push_back(construct[W] + value[W]);
+        for (int i = 1; i <= N; ++i)
+        {
+            child[i] = 0, construct[i] = 0;
+            parent[i].clear();
+        }
+    }
 
-		result.push_back(construct[finale] + value[finale]);
-
-		clear();
-	}
-
-	for (auto p : result)
-	{
-		std::cout << p << "\n";
-	}
+    for (auto p : result)
+    {
+        std::cout << p << "\n";
+    }
 }
