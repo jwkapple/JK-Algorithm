1a4e9f2029d3f31b966358369207dc25aadcf7ab
DESKTOP-H0JLFMK\Glacier's Desktop <jwkorange@gmail.com>
2021-10-10 19:08:08 +0900

    [Update] 11728 Optimized

--- src/BaekJoon/11728.cpp
+++ src/BaekJoon/11728.cpp
@@ -1,58 +1,43 @@
 #include <iostream>
 
+void Init()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL); std::cout.tie(NULL);
+}
+
 const int MAX = 1e6 + 1;
 
-int A[MAX];
-int B[MAX];
-int result[MAX * 2];
-
-int N, M;
+int N, M, a = 0, b = 0;
+int A[MAX], B[MAX];
 
 int main()
 {
-	std::ios_base::sync_with_stdio(false);
+    Init();
 
-	std::cin >> N >> M;
+    std::cin >> N >> M;
 
-	for (int i = 0; i < N; i++)
-	{
-		std::cin >> A[i];
-	}
+    A[N] = 1e9 + 1;
+    B[M] = 1e9 + 1;
 
-	for (int i = 0; i < M; i++)
-	{
-		std::cin >> B[i];
-	}
+    for (int i = 0; i < N; ++i)
+    {
+        std::cin >> A[i];
+    }
+    for (int i = 0; i < M; ++i)
+    {
+        std::cin >> B[i];
+    }
 
-	int a = 0, b = 0;
-	int r = 0;
-
-	while (a < N && b < M)
-	{
-		if (A[a] < B[b])
-		{
-			result[r++] = A[a++];
-		}
-
-		else
-		{
-			result[r++] = B[b++];
-		}
-	}
-
-	while (a < N)
-	{
-		result[r++] = A[a++];
-	}
-
-	while (b < M)
-	{
-		result[r++] = B[b++];
-	}
-
-	for (int i = 0; i < r; i++)
-	{
-		std::cout << result[i] << " ";
-	}
-
+    while (a != N || b != M)
+    {
+        if (A[a] < B[b] || b == M)
+        {
+            std::cout << A[a++] << " ";
+        }
+        else
+        {
+            std::cout << B[b++] << " ";
+        }
+    }
 }
