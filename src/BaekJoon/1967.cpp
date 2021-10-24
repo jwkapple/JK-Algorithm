c66a55c720c7c85f5f015bc478255b5edb84b997
DESKTOP-H0JLFMK\Glacier's Desktop <jwkorange@gmail.com>
2021-10-24 12:58:27 +0900

    [Update] 1967 Optimized

--- src/BaekJoon/1967.cpp
+++ src/BaekJoon/1967.cpp
@@ -5,50 +5,51 @@
 
 void Init()
 {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL); std::cout.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL); std::cout.tie(NULL);
 }
 
 const int MAX = 1e4 + 1;
 
 std::vector<pii> data[MAX];
-
-int N, result = 0, total, value;
+int N, max = 0;
 
 int func(int num)
 {
-    if (!(data[num].size())) return 0;
+	if (data[num].empty()) return 0;
 
-    int first = 0, second = 0;
-    for (auto p : data[num])
-    {
-        auto [to, v] = p;
+	int first = 0, second = 0;
 
-        value = v + func(to);
-        if (value > first) { second = first, first = value; continue; }
-        if (value > second) { second = value; }
-    }
+	int pLong = 0;
+	for (auto p : data[num])
+	{
+		auto [child, value] = p;
+		pLong = func(child) + value;
+		if (pLong > first) { second = first, first = pLong; continue; }
+		if (pLong > second) { second = pLong; }
+	}
 
-    total = first + second;
-    result = result > total ? result : total;
+	pLong = first + second;
 
-    return first;
+	max = max > pLong ? max : pLong;
+
+	return first;
 }
 int main()
 {
-    Init();
+	Init();
 
-    std::cin >> N;
+	std::cin >> N;
 
-    int from, to, v;
-    for (int i = 0; i < N - 1; ++i)
-    {
-        std::cin >> from >> to >> v;
+	int from, to, v;
+	for (int i = 0; i < N - 1; ++i)
+	{
+		std::cin >> from >> to >> v;
 
-        data[from].push_back(pii(to, v));
-    }
+		data[from].push_back(pii(to, v));
+	}
 
-    func(1);
+	func(1);
 
-    std::cout << result;
+	std::cout << max;
 }
