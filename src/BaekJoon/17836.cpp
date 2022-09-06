0261a13e3a0455f28b1cb1f000a76d24c15e900e
jwkapple <jwkorange@gmail.com>
2022-09-06 12:05:28 +0900

    [Add] 17836 BFS

--- src/BaekJoon/17836.cpp
+++ src/BaekJoon/17836.cpp
@@ -6,100 +6,188 @@
 void Init()
 {
 	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL); std::cout.tie(NULL);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
 }
 
-int Dir[4][2]{ {-1, 0},{1, 0},{0, -1}, {0,1} };
-enum Case { NONE = 0, WALL, KEN };
-
+int dir[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 const int MAX = 100 + 1;
 
-bool map[MAX][MAX], visited[MAX][MAX], kVisit[MAX][MAX], active;
+bool visitedM[MAX][MAX], visited[MAX][MAX];
 std::queue<pii> Q;
-int H, W, T, kY, kX;
-int sResult = 1e9, kResult = 1e9;
+int data[MAX][MAX];
+int N, M, T, mY, mX;
 
 int main()
 {
 	Init();
 
-	std::cin >> H >> W >> T;
+	std::cin >> N >> M;
 
-	int tmp;
-	for (int y = 1; y <= H; ++y)
+	for (int y = 1; y <= N; ++y)
 	{
-		for (int x = 1; x <= W; ++x)
+		for (int x = 1; x <= M; ++x)
 		{
-			std::cin >> tmp;
+			std::cin >> data[y][x];
 
-			if (tmp == KEN) { kY = y, kX = x; }
-			else map[y][x] = tmp;
+			if (data[y][x] == 2)
+			{
+				mY = y;
+				mX = x;
+			}
 		}
 	}
 
 	Q.push(pii(1, 1));
 
-	int num = 0, size, Y, X;
-	while (!Q.empty() && !sResult)
+	bool isDone = false;
+
+	int maken = 0;
+
+	std::queue<pii> mQ;
+	while (!mQ.empty() && !isDone)
 	{
-		size = Q.size();
-
-		for (int a = 0; a < size; ++a)
-		{
-			auto[y, x] = Q.front(); Q.pop();
-
-			if (visited[y][x]) continue;
-			visited[y][x] = true;
-
-			if (y == H && x == W)
-			{
-				sResult = num;
-				active = true;
-				break;
-			}
-
-			if (y == kY && x == kX) kResult = num;
-
-			for (int i = 0; i < 4; ++i)
-			{
-				Y = y + Dir[i][0];
-				X = x + Dir[i][1];
-
-				if (!visited[Y][X]) Q.push(pii(Y, X));
-			}
-		}
-		num++;
-	}
-
-	while (!Q.empty()) { Q.pop(); }
-
-	bool done = false;
-	Q.push(pii(kY, kX));
-	num = 0;
-	while (!Q.empty() && !done)
-	{
-		size = Q.size();
+		int size = mQ.size();
 
 		for (int i = 0; i < size; ++i)
 		{
-			auto[y, x] = Q.front(); Q.pop();
+			auto [y, x] = mQ.front();
+			mQ.pop();
 
-			if (kVisit[y][x]) continue;
-			kVisit[y][x] = true;
+			if (visitedM[y][x])
+				continue;
 
-			if (y == H && x == W)
+			visitedM[y][x] = true;
+
+			if (data[y][x] == 2)
 			{
-				kResult += num;
-				active = true;
-				done = true;
+				isDone = true;
+				break;
+			}
+
+			for (int j = 0; j < 4; ++j)
+			{
+				int Y = y + dir[j][0];
+				int X = x + dir[j][0];
+
+				if (Y <= N && Y > 0 && X <= M && X > 0)
+				{
+					if (!visited[Y][X])
+						mQ.push(pii(Y, X));
+				}
+			}
+		}
+
+		maken++;
+	}
+
+	if (isDone)
+		maken--;
+	else
+		maken = -1;
+
+	for (int y = 1; y <= N; ++y)
+	{
+		for (int x = 1; x <= M; ++x)
+		{
+			visitedM[y][x] = false;
+		}
+	}
+	std::queue<pii> mmQ;
+	mmQ.push(pii(mY, mX));
+
+	isDone = false;
+
+	int mToC = 0;
+	while (!mmQ.empty() && !isDone)
+	{
+		int size = mmQ.size();
+
+		for (int i = 0; i < size; ++i)
+		{
+			auto [y, x] = mmQ.front();
+			mmQ.pop();
+
+			if (visitedM[y][x])
+				continue;
+
+			visitedM[y][x] = true;
+
+			if (y == N && x == M)
+			{
+				isDone = true;
 				break;
 			}
 		}
-		num++;
+
+		mToC++;
 	}
 
-	if (!active) std::cout << "Fail";
+	if (isDone)
+		mToC--;
+	else
+		mToC = -1;
 
-	if (sResult < kResult) std::cout << sResult;
-	else std::cout << kResult;
+	Q.push(pii(1, 1));
+
+	isDone = false;
+
+	int value = 0;
+	while (!Q.empty() && !isDone)
+	{
+		int size = Q.size();
+		Q.pop();
+
+		for (int i = 0; i < size; ++i)
+		{
+			auto [y, x] = Q.front();
+			Q.pop();
+
+			if (visited[y][x])
+				continue;
+
+			visited[y][x] = true;
+
+			if (y == N && x == M)
+			{
+				isDone = true;
+				break;
+			}
+		}
+
+		value++;
+	}
+
+	if (isDone)
+		value--;
+	else
+		value = -1;
+
+	if (maken == -1 || mToC == -1)
+	{
+		if (value == -1)
+		{
+			std::cout << "Fail";
+		}
+		else
+		{
+			if (value <= T)
+			{
+				std::cout << value;
+			}
+			else
+				std::cout << "Fail";
+		}
+	}
+	else
+	{
+		int result = maken + mToC;
+
+		result = result > value ? value : result;
+
+		if (result <= T)
+			std::cout << result;
+		else
+			std::cout << "Fail";
+	}
 }
