b351f198cd01ce5822d8cb385fff94978da6848a
jwkapple <jwkorange@gmail.com>
2022-10-11 01:36:51 +0900

    [Add] 16198 Simulation

--- src/BaekJoon/11058.cpp
+++ src/BaekJoon/11058.cpp
@@ -9,19 +9,23 @@ void Init()
 	std::cin.tie(NULL); std::cout.tie(NULL);
 }
 
-int MAX = 100 + 1;
+const int MAX = 100 + 1;
 int N;
 ll data[MAX];
 
-int main(int argc, char* argv[]) {
-
+int main() 
+{
 	Init();
 
 	std::cin >> N;
 
-	for (int i = 0; i <= 5; ++i) { data[i] = i; }
+	for (int i = 0; i <= 5; ++i) 
+	{ 
+		data[i] = i; 
+	}
 
-	for (int i = 6; i <= N; i++) {
+	for (int i = 6; i <= N; i++) 
+	{
 		data[i] = std::max({ data[i - 1] + 1, data[i - 3] * 2, data[i - 4] * 3, data[i - 5] * 4 });
 	}
 
